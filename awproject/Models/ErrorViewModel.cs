a2236382d56ef612bd5f13040e85b38b7a4bb68e
Alex <alexwerner569@hotmail.com>
2022-09-30 18:25:06 -0700

    initial commit

---
+++ awproject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace awproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
