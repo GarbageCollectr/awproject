d7c35ef8515e9631399b5d50ca0475e4d637ed07
Alex <alexwerner569@hotmail.com>
2022-10-16 19:38:01 -0700

    exam and question model made, needs more work

---
+++ awprojectmodels/Exam.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace awprojectmodels
+{
+    public class Exam
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ExamBank { get; set; } = null!;
+        public DateTime DateTime { get; set; }
+        public float Score { get; set; }
+        public List<Question> ExamQuestions { get; set; }
+    }
+}
---
+++ awprojectmodels/Question.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace awprojectmodels
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ExamBank { get; set; } = null!;
+        [Required(ErrorMessage = "Question is Required")]
+        public string ExamQuestion { get; set; } = null!;
+        [Required(ErrorMessage = "Answers are Required")]
+        public List<string> Answers { get; set; } = null!;
+        [Required(ErrorMessage = "Correct Answer is Required")]
+        public int CorrectAnswer { get; set; }
+        public string? SubjectDomain { get; set; }
+    }
+}
